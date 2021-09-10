e4f4f9a91d5b04938b3644c8060c8b4ed41c1826
dasitha98 <dasitha98@gmail.com>
2021-09-10 22:53:55 +0530

    frontend/ import js files into app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React from 'react';
 import './App.css';
 import Home from './componend/home';
 
+
+
+import Navbar from './navbar';
+import CreateProducts from './create/createpromotion';
+import ListProducts from './list/listspromotion';
+import UpdateProducts from './update/updatepromotion';
+// import Footer from './footer/footer';
+import Bar from './bar/bar';
+
+
+
+
+
 function App() {
   return (
     <React.Fragment>
